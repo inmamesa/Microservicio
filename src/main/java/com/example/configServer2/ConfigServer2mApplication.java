64573071dd0d4bc17297ac0035df0eb87b61afd7
inmamesa <inmamesa98@gmail.com>
2021-11-29 14:45:26 +0100

    Inicial commit

---
+++ src/main/java/com/example/configServer2/ConfigServer2mApplication.java
@@ -0,0 +1,13 @@
+package com.example.configServer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer2mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer2mApplication.class, args);
+	}
+
+}
